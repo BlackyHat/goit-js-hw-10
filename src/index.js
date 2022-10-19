05b02894843c87c834f5bccadd076afeb0faade9
Oleksandr P <goodwin009@gmail.com>
2022-10-19 13:35:55 +0300

    add js oop

--- src/index.js
+++ src/index.js
@@ -3,49 +3,39 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries.js';
 
-const DEBOUNCE_DELAY = 300;
-const refs = {
-  input: document.querySelector('#search-box'),
-  elCountriesList: document.querySelector('.country-list'),
-  elInfoCountryCard: document.querySelector('.country-info'),
-};
-
-refs.input.addEventListener('input', debounce(find, DEBOUNCE_DELAY));
-
-function find(e) {
-  const inutvalue = e.target.value.trim();
-  if (inutvalue) {
-    fetchCountries(inutvalue).then(checkAnswerArrLength).catch(getError);
+class Country {
+  constructor({ input, elCountriesList, elInfoCountryCard, DEBOUNCE_DELAY }) {
+    this.input = input;
+    this.elCountriesList = elCountriesList;
+    this.elInfoCountryCard = elInfoCountryCard;
+    this.DEBOUNCE_DELAY = DEBOUNCE_DELAY;
   }
-}
 
-function getError() {
-  clearFindedList();
-  return Notify.failure('Oops, there is no country with that name');
-}
-
-function checkAnswerArrLength(arr) {
-  if (arr.length) {
-    if (arr.length <= 10 && arr.length >= 2) {
-      return markupCountriesList(arr);
-    }
-    if (arr.length > 10) {
-      return Notify.info(
-        'Too many matches found. Please enter more specific name.'
-      );
-    }
-    return markupCountryInfo(arr);
+  markupCountriesList(countries) {
+    const markup = countries
+      .map(
+        el => `<li class='country-list__item'>
+      <img
+        class='country__flag'
+        src='${el.flags.svg}'
+        alt='${el.name.official}'
+      />
+      <p class='country__label'>${el.name.common}</p>
+    </li>
+        `
+      )
+      .join('');
+    refs.elInfoCountryCard.innerHTML = '';
+    refs.elCountriesList.innerHTML = markup;
   }
-  return error;
-}
 
-function markupCountryInfo(countries) {
-  const countryLangValues = countries.map(el =>
-    Object.values(el.languages).join(', ')
-  );
+  markupCountryInfo(countries) {
+    const countryLangValues = countries.map(el =>
+      Object.values(el.languages).join(', ')
+    );
 
-  const markup = countries.map(
-    el => `<div class='country__info'>
+    const markup = countries.map(
+      el => `<div class='country__info'>
   <div class='country__info-wrap'>
     <img
       class='country__info-flag'
@@ -62,31 +52,66 @@ function markupCountryInfo(countries) {
      </span> ${countryLangValues}
     </p>
 </div>`
-  );
+    );
 
-  refs.elCountriesList.innerHTML = '';
-  refs.elInfoCountryCard.innerHTML = markup;
+    this.elCountriesList.innerHTML = '';
+    this.elInfoCountryCard.innerHTML = markup;
+  }
+
+  getError() {
+    this.clearFindedList.bind(this)();
+    return Notify.failure('Oops, there is no country with that name');
+  }
+
+  checkAnswerArrLength(arr) {
+    this.clearFindedList.bind(this)();
+
+    if (arr.length) {
+      if (arr.length <= 10 && arr.length >= 2) {
+        return this.markupCountriesList.bind(this)(arr);
+      }
+
+      if (arr.length > 10) {
+        this.clearFindedList.bind(this)();
+        return Notify.info(
+          'Too many matches found. Please enter more specific name.'
+        );
+      }
+      return this.markupCountryInfo.bind(this)(arr);
+    }
+    return error;
+  }
+
+  clearFindedList() {
+    this.elCountriesList.innerHTML = '';
+    this.elInfoCountryCard.innerHTML = '';
+  }
+
+  find(e) {
+    const inutvalue = e.target.value.trim();
+    if (inutvalue) {
+      fetchCountries(inutvalue)
+        .then(this.checkAnswerArrLength.bind(this))
+        .catch(this.getError.bind(this));
+    }
+  }
+  addListeners() {
+    this.input.addEventListener(
+      'input',
+      debounce(this.find.bind(this), this.DEBOUNCE_DELAY)
+    );
+  }
+
+  init() {
+    this.addListeners();
+  }
 }
 
-function markupCountriesList(countries) {
-  const markup = countries
-    .map(
-      el => `<li class='country-list__item'>
-      <img
-        class='country__flag'
-        src='${el.flags.svg}'
-        alt='${el.name.official}'
-      />
-      <p class='country__label'>${el.name.common}</p>
-    </li>
-        `
-    )
-    .join('');
-  refs.elInfoCountryCard.innerHTML = '';
-  refs.elCountriesList.innerHTML = markup;
-}
+const refs = {
+  input: document.querySelector('#search-box'),
+  elCountriesList: document.querySelector('.country-list'),
+  elInfoCountryCard: document.querySelector('.country-info'),
+  DEBOUNCE_DELAY: 300,
+};
 
-function clearFindedList() {
-  refs.elCountriesList.innerHTML = '';
-  refs.elInfoCountryCard.innerHTML = '';
-}
+new Country(refs).init();
